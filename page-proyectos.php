4924fbdb7e930025a55624ff17d8b1874a1cd60c
daniel <picisdaniel2016@gmail.com>
2025-06-12 03:18:54 -0400

    solo me falta los posts para mis categorias y el crud del formulario

--- page-proyectos.php
+++ page-proyectos.php
@@ -95,9 +95,12 @@
                     01
                 </h3>
                 <p class="font-[DM Sans] font-normal md:text-lg pb-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-                    corrupti laudantium nam cumque amet totam quasi temporibus minima
-                    alias optio. sladfsakld sadfsd sdfsdfsd
+                    Antes de iniciar un sitio web, es importante realizar una investigación
+                    previa que permita optimizar su funcionamiento. Herramientas como el <span class="text-[#FF6F61] font-semibold">Brief</span>
+                    ayudan a definir el enfoque estratégico analizando al público objetivo,
+                    mientras que el <span class="text-[#FF6F61] font-semibold">Benchmarking</span> permite evaluar la usabilidad, accesibilidad
+                    y estética de sitios similares. Aplicar estos métodos favorece el desarrollo
+                    de una estructura clara y alineada con las necesidades del usuario.
                 </p>
             </div>
             <div
@@ -106,9 +109,15 @@
                     02
                 </h3>
                 <p class="font-[DM Sans] font-normal md:text-lg pb-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-                    corrupti laudantium nam cumque amet totam quasi temporibus minima
-                    alias optio.
+                    Es fundamental crear un <span class="text-[#FF6F61] font-semibold">Wireframe</span>
+                    para planificar su estructura y distribución visual. Herramientas como
+                    Figma o Adobe XD, junto con plugins, facilitan este proceso y lo hacen
+                    más dinámico.
+                </p>
+                <p class="font-[DM Sans] font-normal md:text-lg pb-2">
+                    Al diseñar el <span class="text-[#FF6F61] font-semibold">Mockup</span> final, se debe aplicar lo aprendido
+                    para definir tipografías, colores, tamaños de imagen y secciones,
+                    asegurando así un diseño claro, coherente y funcional desde el inicio.
                 </p>
             </div>
             <div
@@ -117,9 +126,15 @@
                     03
                 </h3>
                 <p class="font-[DM Sans] font-normal md:text-lg pb-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-                    corrupti laudantium nam cumque amet totam quasi temporibus minima
-                    alias optio.
+                    Antes de comenzar a codificar con <span class="text-[#FF6F61] font-semibold">HTML</span> y <span class="text-[#FF6F61] font-semibold">CSS</span>, es clave definir la estructura del sitio,
+                    pestañas, enlaces y librerías necesarias. Esto permite organizar mejor
+                    los recursos, facilitar el uso de <span class="text-[#FF6F61] font-semibold">JavaScript</span> y mantener ordenados los
+                    archivos en carpetas.
+                </p>
+                <p class="font-[DM Sans] font-normal md:text-lg pb-2">
+                    Con todo listo y descargado, se inicia la etapa
+                    final de codificación, aplicando lo planificado para construir el sitio
+                    con un diseño responsivo y bien estructurado desde el principio.
                 </p>
             </div>
             <div
@@ -128,9 +143,15 @@
                     04
                 </h3>
                 <p class="font-[DM Sans] font-normal md:text-lg pb-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-                    corrupti laudantium nam cumque amet totam quasi temporibus minima
-                    alias optio.
+                    Subimos el proyecto a <span class="text-[#FF6F61] font-semibold">WordPress</span>, primero se configura en local usando
+                    MAMP o XAMPP, descargando WordPress y creando una base de datos en <span class="text-[#FF6F61] font-semibold">PhpMyAdmin</span>,
+                    ubicada en la carpeta htdocs. Luego, se agrega el proyecto en wp-content/themes/(nombre del sitio)
+                    con archivos clave como style.css, y el lenguaje Backend como <span class="text-[#FF6F61] font-semibold">PHP</span> para index.php y functions.php para estructurar el sitio.
+                </p>
+                <p class="font-[DM Sans] font-normal md:text-lg pb-2">
+                    Después se organiza el contenido en categorías, entradas y páginas para completar la estructura.
+                    Finalmente, el sitio se sube a producción mediante CPanel, importando la base de datos y
+                    vinculándolo correctamente al dominio para su publicación online.
                 </p>
             </div>
         </div>
