fab8d776df8f5f9b287d2644ad676d90a2fcdfcc
daniel <picisdaniel2016@gmail.com>
2025-06-16 18:03:00 -0400

    agregue el favicon

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo get_bloginfo("name") ?></title>
+    <link rel="shortcut icon" href="<?php echo get_theme_file_uri('./img/logo.svg') ?>" type="image/x-icon" />
     <?php wp_head(); ?>
 </head>
 
